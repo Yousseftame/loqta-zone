98ac8550c86817a3c898add3040fb493eb9a8d8a
Yousseftame <tamerosama73@gmail.com>
2026-03-02 11:48:30 +0200

    setup localiztion

---
+++ src/hooks/useLanguage.ts
@@ -0,0 +1,24 @@
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+
+export const useLanguage = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lang :any) => {
+    i18n.changeLanguage(lang);
+    document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+    document.documentElement.lang = lang;
+  };
+
+  // Sync dir on initial load
+  useEffect(() => {
+    document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
+
+  return {
+    currentLang: i18n.language,
+    changeLanguage,
+    isRTL: i18n.language === 'ar',
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './lib/i18n.ts'; // 
 import './index.css'
 import App from './App.tsx'
 import ClickSpark from './components/ClickSpark.tsx';
